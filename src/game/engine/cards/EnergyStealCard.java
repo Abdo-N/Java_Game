0cce9e9782885a3570f631cfca4e0417eaa7f199
Hatem-Mourtaga <mourtagahatem7@gmail.com>
2026-04-28 23:59:58 +0300

    p3 part of the task(not completed yet)

--- src/game/engine/cards/EnergyStealCard.java
+++ src/game/engine/cards/EnergyStealCard.java
@@ -1,6 +1,7 @@
 package game.engine.cards;
 
 import game.engine.interfaces.CanisterModifier;
+import game.engine.monsters.Monster;
 
 public class EnergyStealCard extends Card implements CanisterModifier {
 	private int energy;
@@ -12,6 +13,11 @@ public class EnergyStealCard extends Card implements CanisterModifier {
 	
 	public int getEnergy() {
 		return energy;
+	}
+	public void performAction(Monster player, Monster opponent){
+		int stolenAmount = Math.min(energy, opponent.getEnergy());
+		opponent.alterEnergy(-stolenAmount);
+		player.setEnergy(player.getEnergy() + stolenAmount);
 	}
 	
 }
